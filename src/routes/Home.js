94dee7ce385e50df5c2a6ba943141a2cf8214b9d
LucasElijah <elijahlucas96@gmail.com>
2023-01-08 11:28:18 -0800

    homejs

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Footer from '../components/Footer/Footer';
 import HeroImg from '../components/HeroImg/HeroImg';
-import Work from '../components/Work';
+import Work from '../components/Work/Work';
 import Navbar from '../components/Navbar';
 
 const Home = () => {
