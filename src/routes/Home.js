f492484988167e6ce8d609c2a33c9cec9b436e98
LucasElijah <elijahlucas96@gmail.com>
2023-01-09 10:50:05 -0800

    Update Home.js

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Footer from '../components/Footer/Footer';
 import HeroImg from '../components/HeroImg/HeroImg';
 import Work from '../components/Work/Work';
-import Navbar from '../components/Navbar';
+import Navbar from '../components/Navbar/Navbar';
 
 const Home = () => {
   return (
