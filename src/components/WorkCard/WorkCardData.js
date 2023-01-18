45bb29032e3fec894552e66d252ed34d13755df2
LucasElijah <elijahlucas96@gmail.com>
2023-01-18 13:51:59 -0800

    Update WorkCardData.js

--- src/components/WorkCard/WorkCardData.js
+++ src/components/WorkCard/WorkCardData.js
@@ -1,6 +1,6 @@
 import pro1 from "../../assets/pro1.jpg";
 import pro2 from "../../assets/pro2.jpg";
-import pro3 from "../assets/pro3.jpg";
+import pro3 from "../../assets/pro3.jpg";
 
 const ProjectCardData= [
   {
